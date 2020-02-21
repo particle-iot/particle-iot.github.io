67fc343a8f5d6fbdd902fbb2581064b953beff4f
rickkas7 <rickk@rickk.com>
2020-02-21 06:28:21 -0500

    Add CellularHelper

---
+++ CellularHelper/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['cellular_20helper_0',['Cellular Helper',['../index.html',1,'']]]
+];
---
+++ CellularHelper/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['cellular_20helper_1',['Cellular Helper',['../index.html',1,'']]]
+];
